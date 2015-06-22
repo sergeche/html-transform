292a57ae3eae22c2c40e226f929c3393f3fca82c
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-22 17:44:17 +0300

    Minor refactoring

--- index.js
+++ index.js
@@ -8,34 +8,46 @@ var bufferPipeline = require('./lib/buffer');
 var streamPipeline = require('./lib/stream');
 
 /**
- * Returns a Transofrm stream for VinylFS file object
+ * Returns a Transform stream for VinylFS file object
  * @return {stream.Transform}
  */
 module.exports = function(options) {
 	options = options || {};
 
 	return through.obj(function(file, enc, next) {
-		if (file.isNull()) {
-			return next(null, file);
-		}
-
-		var input = rewriteUrl(options);
-		var output = input;
-
-		// use custom transformers, if provided
-		if (options.transform) {
-			var t = Array.isArray(options.transform) ? options.transform : [options.transform];
-			t.forEach(function(stream) {
-				output = output.pipe(stream);
-			});
-		}
-
-		var transform = duplexer(input, output);
-
-		if (file.isStream()) {
-			return streamPipeline(file, transform, options, next);
-		}
-
-		bufferPipeline(file, transform, options, next);
+		return module.exports.process(file, options, next);
 	});
+};
+
+/**
+ * Processes single VinylFS file
+ * @param  {VinylFS}   file
+ * @param  {Function} next
+ */
+module.exports.process = function(file, options, next) {
+	options = options || {};
+
+	if (file.isNull()) {
+		return next(null, file);
+	}
+
+	var input = rewriteUrl(options);
+	var output = input;
+
+	// use custom transformers, if provided
+	// each `transform` entry must be a function that returns a transform stream
+	if (options.transform) {
+		var t = Array.isArray(options.transform) ? options.transform : [options.transform];
+		t.forEach(function(streamFactory) {
+			output = output.pipe(streamFactory(options));
+		});
+	}
+
+	var transform = duplexer(input, output);
+
+	if (file.isStream()) {
+		return streamPipeline(file, transform, options, next);
+	}
+
+	return bufferPipeline(file, transform, options, next);
 };
