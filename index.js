9e20ce3afccb6ac7e9bdef8e67018f2fe59bb89f
Sergey Chikuyonok <serge.che@gmail.com>
2016-06-07 17:58:23 +0300

    Expose `fileStats` module

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const htmlparser = require('htmlparser2');
 const rewriteUrl = require('./lib/rewrite-url');
 const parseDom = require('./lib/parse-dom');
 const stringifyDom = require('./lib/stringify');
+const fileStats = require('./lib/file-stats');
 
 // Currently, only `rewriteUrl` process is supported. In future it might be
 // replaced with more complex process
@@ -14,3 +15,4 @@ module.exports.htmlparser = htmlparser;
 module.exports.parseDom = parseDom;
 module.exports.rewriteUrl = rewriteUrl;
 module.exports.stringifyDom = stringifyDom;
+module.exports.fileStats = fileStats;
